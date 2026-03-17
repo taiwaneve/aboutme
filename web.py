7c6ecab6b74afea9344bd1b576173a9b25c04062
taiwaneve <96127592+taiwaneve@users.noreply.github.com>
2026-03-17 11:44:29 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -15,5 +15,21 @@ def course():
 def today():
     now = datetime.now()
     return render_template("today.html", datetime = str(now))
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    user = request.values.get("nick")
+    return render_template("welcome.html", name=user)
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
 if __name__ == "__main__":
     app.run()
